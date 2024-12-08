fbaa1c30929df045745d4a3b3b7d8af0da16ad1e
ToadTraveller <toad.traveller@gmail.com>
2024-12-08 23:41:58 +0100

    Foo bez pętli

---
+++ build/CMakeFiles/zal.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bimberiev/lab-6-ToadTraveller/test.cpp" "CMakeFiles/zal.dir/test.cpp.o" "gcc" "CMakeFiles/zal.dir/test.cpp.o.d"
+  "/home/bimberiev/lab-6-ToadTraveller/test_main.cpp" "CMakeFiles/zal.dir/test_main.cpp.o" "gcc" "CMakeFiles/zal.dir/test_main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/zal.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zal.dir/test.cpp.o"
+  "CMakeFiles/zal.dir/test.cpp.o.d"
+  "CMakeFiles/zal.dir/test_main.cpp.o"
+  "CMakeFiles/zal.dir/test_main.cpp.o.d"
+  "zal"
+  "zal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
